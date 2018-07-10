327c6191ca838e3a32c16c03098e01856d61690d
Viacheslav Shatan <sha02viacheslav@gmail.com>
2018-07-10 18:19:32 +0200

    Update Mailgun Domain

--- api/config/index.js
+++ api/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   FRONTEND_URI:
     process.env.FRONTEND_URI ||
     (process.env.NODE_ENV === 'production'
-      ? 'http://159.203.178.177'
+      ? 'https://bidblab.com'
       : 'http://localhost:4200'),
   SECRET:
     process.env.SECRET ||
@@ -16,7 +16,10 @@ module.exports = {
     from: 'Bidblab <support@bidblab.com>',
     auth: {
       api_key: '1c483f030a25d74004bd2083d3f42585-b892f62e-b1b60d12',
-      domain: 'sandbox172690e9cd1e44c385681721a74d37fa.mailgun.org',
+      domain:
+        process.env.NODE_ENV === 'production'
+          ? 'verify.bidblab.com'
+          : 'sandbox172690e9cd1e44c385681721a74d37fa.mailgun.org',
     },
   },
   MEDIA_FOLDER: 'uploads',
