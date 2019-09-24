09d2362a166da3d53355e3c4961a8963ef490399
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-09-24 18:02:33 +0300

    fix cors

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const app = express();
 app.set('secret', config.SECRET);
 
 app.use(logger(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
-const origins = process.env.NODE_ENV === 'production' ? ['https://bidblab.com', 'https://www.bidblab.com'] 
+const origins = process.env.NODE_ENV === 'production' ? ['https://bidblab.com', 
+		'https://www.bidblab.com', 'https://admin.bidblab.com'] 
 	: ['http://localhost:4300', 'http://localhost:4200', 'http://localhost:4400'];
 app.use(
 	cors({
