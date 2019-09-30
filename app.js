2e0574105956499ddc66a69e555da6ffc0a182be
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-10-01 02:02:35 +0300

    change cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('secret', config.SECRET);
 app.use(logger(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 const origins = process.env.NODE_ENV === 'production' ? ['https://bidblab.com', 
 		'https://www.bidblab.com', 'https://admin.bidblab.com'] 
-	: ['http://localhost:4300', 'http://localhost:4200', 'http://localhost:4400'];
+	: ['http://localhost:4300', 'http://localhost:4200', 'http://localhost:4000'];
 app.use(
 	cors({
 		origin: origins,
